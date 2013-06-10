292cdafcc3760f1f876c328a2469886045ff7e98
Dejan REBRACA <dejanx.rebraca@intel.com>
2013-06-10 18:39:30 +0200

    Make sure that return callback is called in phLibNfc_SE_SetMode
    BZ: 110743
    
    Even a mutex protection is done in LLC layer, it can still happen that
    write thread is preempted by response callback thread during rolling
    back of the call stack back to upper JNI layer. Here we make sure that
    in this preemption scenario we already have callback info structure
    already updated.
    
    Change-Id: I4a9267f2a5f2da947e712a10b61d06950e602910
    Signed-off-by: Dejan REBRACA <dejanx.rebraca@intel.com>
    Reviewed-on: http://android.intel.com:8080/112824
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Niciarz, Sebastian <sebastian.niciarz@intel.com>
    Tested-by: Zurmely, PierreX <pierrex.zurmely@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- src/phLibNfc_SE.c
+++ src/phLibNfc_SE.c
@@ -470,6 +470,11 @@ NFCSTATUS phLibNfc_SE_SetMode ( phLibNfc_Handle             hSE_Handle,
     }
     else 
     {
+        pLibContext->sSeContext.hSetemp=hSE_Handle;
+        pLibContext->status.GenCb_pending_status = TRUE;
+        pLibContext->sSeContext.sSeCallabackInfo.pSEsetModeCb = pSE_SetMode_Rsp_cb;
+        pLibContext->sSeContext.sSeCallabackInfo.pSEsetModeCtxt=pContext;
+
         switch(eActivation_mode)
         {
             case phLibNfc_SE_ActModeVirtual: 
@@ -642,20 +647,17 @@ NFCSTATUS phLibNfc_SE_SetMode ( phLibNfc_Handle             hSE_Handle,
                 break;
 
         }/*End of eActivation_mode switch */       
-        if(Status==NFCSTATUS_PENDING)
+
+        if(Status != NFCSTATUS_PENDING)
         {
-            pLibContext->sSeContext.hSetemp=hSE_Handle;
-            pLibContext->status.GenCb_pending_status = TRUE;
-            pLibContext->sSeContext.sSeCallabackInfo.pSEsetModeCb = pSE_SetMode_Rsp_cb;
-            pLibContext->sSeContext.sSeCallabackInfo.pSEsetModeCtxt=pContext;                       
-        }
-        else if(Status == NFCSTATUS_INVALID_HANDLE)
-        {
-            Status= Status;
-        }
-        else
-        {
-            Status = NFCSTATUS_FAILED;
+            pLibContext->status.GenCb_pending_status = FALSE;
+            pLibContext->sSeContext.sSeCallabackInfo.pSEsetModeCb = NULL;
+            pLibContext->sSeContext.sSeCallabackInfo.pSEsetModeCtxt = NULL;
+
+            if (Status != NFCSTATUS_INVALID_HANDLE) {
+                Status = NFCSTATUS_FAILED;
+            }
+
         }
     }
     return Status;
